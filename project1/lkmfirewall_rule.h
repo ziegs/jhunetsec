fd293c2f4fb1f27af4e109edd57ed73537eed49b
Matthew Ziegelbaum <ziegs@ubuntu.(none)>
2010-02-10 16:30:37 -0500

    fix a couple bugs in firewall rule header

--- project1/lkmfirewall_rule.h
+++ project1/lkmfirewall_rule.h
@@ -3,28 +3,31 @@
  *
  *  Created on: Feb 10, 2010
  *      Author: Ian Miers
+ *      Author: Matt Ziegelbaum
  */
 
 #ifndef LKMFIREWALL_RULE_H_
 #define LKMFIREWALL_RULE_H_
+
 #include <linux/types.h>
 #include <linux/list.h>
+
 struct firewall_rule {
 	struct list_head list;
-	enum { IN,OUT} in_or_out;
-	/** what protocal the rule is for */
-	enum{
+	enum {IN, OUT} direction;
+	/** what protocol the rule is for */
+	enum {
 		TCP,
 		UDP,
 		ICMP,
-	} protocal;
-	_be32 src_ip;
-	_be32 src_netmask;
-	_be32 src_port;
+	} protocol;
+	__be32 src_ip;
+	__be32 src_netmask;
+	__be32 src_port;
 
-	_be32 dest_ip;
-	_be32 dest_port;
-	_be32 dest_netmask;
+	__be32 dest_ip;
+	__be32 dest_port;
+	__be32 dest_netmask;
 };
 
 #endif /* LKMFIREWALL_RULE_H_ */
