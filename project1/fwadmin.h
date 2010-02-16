4638cef4a85058fbfe481a7d72a87f4109bb8b99
user <user@ubuntu-9-10-AMD-64-DEV.lan>
2010-02-15 19:47:31 -0500

    adding fwadmin.h

---
+++ project1/fwadmin.h
@@ -0,0 +1,21 @@
+/*
+ * fwadmin.h
+ *
+ *  Created on: Feb 13, 2010
+ *      Author: user
+ */
+#include <linux/types.h>
+#include "lkmfirewall_rule.h"
+#ifndef FWADMIN_H_
+#define FWADMIN_H_
+
+#define checkEnum(enum,name){ if(enum.name ==)}
+int handle_ip(const char * name, const char *ip, __be32 *ip_num,
+		 int printerr);
+int handle_port(const char * name, const char * port, __be32 *port_num,
+		 int printerr);
+int handle_netmask(const char * name, const char * netmask,
+		__be32 *net_mask_num,  int printerr);
+#endif /* FWADMIN_H_ */
+
+void serialize_rule(const struct firewall_rule rule);
